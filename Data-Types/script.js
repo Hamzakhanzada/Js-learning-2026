cb05dab586928361edc2f4f7f8c46d09b25f0e6e
Hamzakhanzada <khanzadahamza76@gmail.com>
2026-02-05 12:23:50 +0500

    chore: Data Types

---
+++ Data-Types/script.js
@@ -0,0 +1,31 @@
+// Data Types 
+
+// There are two kinds of data types 
+// 1 primitive data types.
+// 2 reference data types.
+
+// Primitive esi sari values jin ko copy karna per tuma real copy mila 
+let a = 12;
+let b = a;
+
+a += 2
+
+
+// data types have 7 kinds 
+// 1 string
+// 2 number
+// 3 boolean
+// 4 bigint
+// 5 symbol
+// 6 undefined
+// 7 null
+
+// Reference esi sari values jin ko copy karna per tuma real copy na mila reference mila parent ka 
+//  data types have 3 kinds;
+
+// array objects and functions 
+// [] ,  {} , () 
+// 
+
+
+
