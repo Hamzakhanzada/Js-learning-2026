097851494478fcdda9f654c2f2d77d484d9c9e55
Hamzakhanzada <khanzadahamza76@gmail.com>
2026-03-18 13:23:59 +0500

    learning eventslisteners

--- Event-Listeners/script.js
+++ Event-Listeners/script.js
@@ -8,11 +8,15 @@ p.addEventListener('click', click);
 // how to remove eventListeners
 p.removeEventListener('click',click);
 
-
-
 const input = document.querySelector("input")
 input.addEventListener('input',function (e) {
     if (e.data !== null) {
         console.log(e.data)
     }
+})
+
+const sl = document.querySelector("select");
+const h1 = document.querySelector("h1")
+sl.addEventListener("change",function (det) {
+    h1.textContent = `Your Car is ${det.target.value}`
 })
